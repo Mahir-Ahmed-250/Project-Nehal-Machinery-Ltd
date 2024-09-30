32793745b19989e469073321cbbba69059d97df6
unknown <xahin.mahir@gmail.com>
2024-09-30 21:51:47 +0600

    some change

--- src/Pages/Home/HomeOurCustomers/HomeOurCustomers.jsx
+++ src/Pages/Home/HomeOurCustomers/HomeOurCustomers.jsx
@@ -1,74 +1,64 @@
-import React, { useEffect, useState } from 'react';
-import Title from '../../../Components/Title/Title';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import './HomeOurCustomers.css';
+import React, { useEffect, useState } from "react";
+import Title from "../../../Components/Title/Title";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "./HomeOurCustomers.css";
 
-import { EffectCoverflow, Pagination } from 'swiper/modules';
+import { EffectCoverflow, Pagination } from "swiper/modules";
 const HomeOurCustomers = () => {
-    const [customers, setCustomers] = useState([]);
+  const [customers, setCustomers] = useState([]);
   useEffect(() => {
     fetch("HomeOurCustomer.JSON")
       .then((res) => res.json())
       .then((data) => setCustomers(data));
   }, []);
-    return (
-        <div>
-            <Title title="Our Valued Customers"/>
-            <center className="container">
-            <Swiper
-      slidesPerView={3}
-      spaceBetween={30}
-
-      loop={true}
-      loopFillGroupWithBlank={true}
-      breakpoints={{
-
-          768: {
+  return (
+    <div>
+      <Title title="Our Valued Customers" />
+      <center className="container">
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={10}
+          loop={true}
+          loopFillGroupWithBlank={true}
+          breakpoints={{
+            768: {
               slidesPerView: 1,
               spaceBetween: 0,
-          },
-          1024: {
+            },
+            1024: {
               slidesPerView: 3,
               spaceBetween: 0,
-          },
-      }}
-      effect={"coverflow"}
-      centeredSlides={true}
-      autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-      }}
-
-      centeredSlidesBounds={true}
-      coverflowEffect={{
-          rotate: 20,
-          stretch: 0,
-          depth: 100,
-          modifier: 3.5,
-          slideShadows: true
-      }}
-      pagination={{
-          clickable: true
-      }}
-      onPaginationShow={false}
-      modules={[EffectCoverflow, Pagination]}
-      className="mySwiper"
-     
-      >
-   
-          
-            {customers.map((customer) => (
-                <SwiperSlide key={customer.id}>
-                  <img src={customer.image} height="300px" alt="" />
-                </SwiperSlide>
-              ))}
-          
-    
-      </Swiper>
-            </center>
-        
-        </div>
-    );
+            },
+          }}
+          effect={"coverflow"}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          centeredSlidesBounds={true}
+          coverflowEffect={{
+            rotate: 20,
+            stretch: 0,
+            depth: 50,
+            modifier: 3.5,
+            slideShadows: true,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          onPaginationShow={false}
+          modules={[EffectCoverflow, Pagination]}
+          className="mySwiper">
+          {customers.map((customer) => (
+            <SwiperSlide key={customer.id}>
+              <img src={customer.image} height="300px" width="100%" alt="" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </center>
+    </div>
+  );
 };
 
 export default HomeOurCustomers;
