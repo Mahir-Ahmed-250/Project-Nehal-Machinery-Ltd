df02d2526d3e9e82ddadcd6f689dd6475b7fe456
unknown <xahin.mahir@gmail.com>
2024-10-01 20:18:49 +0600

    some change

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -54,8 +54,8 @@ const Footer = () => {
               </div>
               <div className="col-md-3">
                 <h2>Get In Touch</h2>
-                <p>Address: Malek Tower (5th Floor), Opposite to Farmgate Police Box, Farmgate, Dhaka-1215.</p>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14616.220162505537!2d90.39000659999999!3d23.759395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8a126b29789%3A0xe7aeda8c5c81db08!2sUdvash%20Farmgate%20Branch!5e0!3m2!1sen!2sbd!4v1727790051092!5m2!1sen!2sbd"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <p>Address: House: 03 (5th floor), Road no: 15 Sector: 01, Uttara, Dhaka-1230</p>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29217.91020663183!2d90.34037897910154!3d23.738863200000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8caa7e60837%3A0xf47efcf6f49b50c5!2sSultan&#39;s%20Dine%2C%20Dhanmondi%20Branch!5e0!3m2!1sen!2sbd!4v1727792274181!5m2!1sen!2sbd" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
               </div>
             </div>
           </div>
