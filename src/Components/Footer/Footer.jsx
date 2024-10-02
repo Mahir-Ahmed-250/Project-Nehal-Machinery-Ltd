bbc3711152ca4461e54c3b9c99f0ac4ab470232a
unknown <xahin.mahir@gmail.com>
2024-10-02 13:20:30 +0600

    some change

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import './Footer.css';
 import logo from "../../Assets/logo.png";
@@ -54,8 +56,14 @@ const Footer = () => {
               </div>
               <div className="col-md-3">
                 <h2>Get In Touch</h2>
-                <p>Address: House: 03 (5th floor), Road no: 15 Sector: 01, Uttara, Dhaka-1230</p>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29217.91020663183!2d90.34037897910154!3d23.738863200000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8caa7e60837%3A0xf47efcf6f49b50c5!2sSultan&#39;s%20Dine%2C%20Dhanmondi%20Branch!5e0!3m2!1sen!2sbd!4v1727792274181!5m2!1sen!2sbd" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+               <span>Operational Office:</span> <a style={{textDecoration:"none"}}href="http://" target="_blank" rel="noopener noreferrer">
+                House: 03 (5th floor), Road no: 15 Sector: 01, Uttara, Dhaka-1230</a> <br />
+                <br />
+               <span>Register Office:</span><a style={{textDecoration:"none"}} href="http://" target="_blank" rel="noopener noreferrer"> 67/9, Kakrail, Eastern Mansion (2nd floor) Room: 6, Dhaka-1000</a>
+               <br />
+               <br />
+               <span>Factory:</span><a style={{textDecoration:"none"}} href="http://" target="_blank" rel="noopener noreferrer"> 44 South kunipara, Tejgaon I/A
+               Dhaka-1208.</a>
               </div>
             </div>
           </div>
