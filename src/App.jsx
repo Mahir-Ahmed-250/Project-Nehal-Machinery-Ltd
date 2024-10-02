0708ba384d891c531e0e6eb80e8c6eac0801c205
unknown <xahin.mahir@gmail.com>
2024-10-02 16:41:21 +0600

    footer created

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,34 @@ import "./App.css";
 import NavigationBar from "./Components/NavigationBar/NavigationBar";
 import Home from "./Pages/Home/Home/Home";
 import Footer from "./Components/Footer/Footer";
+import ScrollToTop from "react-scroll-to-top";
+import { FloatingWhatsApp } from "react-floating-whatsapp";
+import logo from "../src/Assets/logo.png";
 
 function App() {
   return (
     <>
+    <ScrollToTop smooth color="#6f00ff" />
       <NavigationBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<>THIS IS AboutUS</>} />
       </Routes>
+      <FloatingWhatsApp
+                phoneNumber="01725046048"
+                accountName="Noman Machinery Ltd"
+                avatar={logo}
+                allowEsc
+                allowClickAway
+                notification
+                notificationSound
+                buttonStyle={{
+                  right: "auto",
+                  marginLeft:"20px"
+                  }}
+                  chatboxClassName="chat"
+              />
       <Footer/>
     </>
   );
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,6 +3,8 @@
 import React from 'react';
 import './Footer.css';
 import logo from "../../Assets/logo.png";
+
+
 const Footer = () => {
     return (
       <>
@@ -14,57 +16,84 @@ const Footer = () => {
               </div>
               <div className="col-md-3">
                 <h2>Help</h2>
-                <p>
-                    Machineries
-                </p>
-                <p>
-                    Molds
-                </p>
-                <p>
-                    Raw Materials
-                </p>
-                <p>
-                    Installation Commission
-                </p>
-                <p>
-                    Consultancy Service
-                </p>
-                <p>
-                    Others
-                </p>
+                <p>Machineries</p>
+                <p>Molds</p>
+                <p>Raw Materials</p>
+                <p>Installation Commission</p>
+                <p>Consultancy Service</p>
+                <p>Others</p>
               </div>
               <div className="col-md-3">
-              <h2>Explore</h2>
-              <p>
-                    Machineries
-                </p>
-                <p>
-                    Molds
-                </p>
-                <p>
-                    Raw Materials
-                </p>
-                <p>
-                    Installation Commission
-                </p>
-                <p>
-                    Consultancy Service
-                </p>
-                <p>
-                    Others
-                </p>
+                <h2>Explore</h2>
+                <p>Machineries</p>
+                <p>Molds</p>
+                <p>Raw Materials</p>
+                <p>Installation Commission</p>
+                <p>Consultancy Service</p>
+                <p>Others</p>
               </div>
               <div className="col-md-3">
                 <h2>Get In Touch</h2>
-               <span>Operational Office:</span> <a style={{textDecoration:"none"}}href="http://" target="_blank" rel="noopener noreferrer">
-                House: 03 (5th floor), Road no: 15 Sector: 01, Uttara, Dhaka-1230</a> <br />
+                <span>Operational Office:</span>{" "}
+                <a
+                  style={{ textDecoration: "none" }}
+                  href="http://"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  House: 03 (5th floor), Road no: 15 Sector: 01, Uttara,
+                  Dhaka-1230
+                </a>{" "}
                 <br />
-               <span>Register Office:</span><a style={{textDecoration:"none"}} href="http://" target="_blank" rel="noopener noreferrer"> 67/9, Kakrail, Eastern Mansion (2nd floor) Room: 6, Dhaka-1000</a>
-               <br />
-               <br />
-               <span>Factory:</span><a style={{textDecoration:"none"}} href="http://" target="_blank" rel="noopener noreferrer"> 44 South kunipara, Tejgaon I/A
-               Dhaka-1208.</a>
+                <br />
+                <span>Register Office:</span>
+                <a
+                  style={{ textDecoration: "none" }}
+                  href="http://"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {" "}
+                  67/9, Kakrail, Eastern Mansion (2nd floor) Room: 6, Dhaka-1000
+                </a>
+                <br />
+                <br />
+                <span>Factory:</span>
+                <a
+                  style={{ textDecoration: "none" }}
+                  href="http://"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {" "}
+                  44 South kunipara, Tejgaon I/A Dhaka-1208.
+                </a>
+                <br />
+                <br />
+                <span>Helpline:</span>
+                <a
+                  style={{ textDecoration: "none" }}
+                  href="tel:+8801725046048"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {" "}
+                  +8801725046048
+                </a>
+                <br />
+                <br />
+                <span>Email:</span>
+                <a
+                  style={{ textDecoration: "none" }}
+                  href="mailto:saiful@nehalml.com"
+                >
+                  {" "}
+                  saiful@nehalml.com
+                </a>
               </div>
+             <center>
+            
+             </center>
             </div>
           </div>
         </div>
