73cdaf3fbab9438676d620323dba942d085513f4
unknown <xahin.mahir@gmail.com>
2024-11-06 21:05:46 +0600

    home banner backend some work done

--- src/Pages/Home/HomeBanner/HomeBanners/HomeBanner.jsx
+++ src/Pages/Home/HomeBanner/HomeBanners/HomeBanner.jsx
@@ -23,104 +23,106 @@ const HomeBanner = () => {
       });
       setBanners(list);
       setLoading(false);
+      console.log(banners);
     });
     return bannerListenerSubscription;
   }, []);
 
   return (
     <>
-      <div
-        style={{
-          paddingBottom: "30px",
-          position: "relative",
-        }}
-      >
-        <Carousel
-          additionalTransfrom={0}
-          arrows={true}
-          autoPlaySpeed={3000}
-          centerMode={false}
-          className=""
-          containerClass=""
-          dotListClass=""
-          draggable
-          focusOnSelect={false}
-          infinite
-          itemClass=""
-          keyBoardControl
-          minimumTouchDrag={80}
-          pauseOnHover
-          renderArrowsWhenDisabled={true}
-          renderButtonGroupOutside={false}
-          renderDotsOutside
-          responsive={{
-            desktop: {
-              breakpoint: {
-                max: 3000,
-                min: 1024,
-              },
-              items: 1,
-            },
-            mobile: {
-              breakpoint: {
-                max: 464,
-                min: 0,
-              },
-              items: 1,
-            },
-            tablet: {
-              breakpoint: {
-                max: 1024,
-                min: 464,
-              },
-              items: 1,
-            },
-          }}
-          rewind={false}
-          rewindWithAnimation={false}
-          rtl={false}
-          shouldResetAutoplay
-          showDots
-          sliderClass=""
-          slidesToSlide={1}
-          swipeable
-        >
-          {banners.map((banner) => (
-            <div key={banner.id}>
-              <img src={banner.img} className="bannerImg" alt="" />
-            </div>
-          ))}
-        </Carousel>
-      </div>
-      {/* <GrFormPrevious
-        onClick={prevHandler}
-        className="bannerBtnPrev"
-        size={50}
-        color="#ffff"
-      />
-      <GrFormNext
-        onClick={nextHandler}
-        className="bannerBtnNext"
-        size={50}
-        color="#ffff"
-      /> */}
-      <div>
-        <center className="animate__animated animate__fadeInUp animate__slow bannerTextSection">
-          <h2 className="bannerText">
-            Welcome to <br />
-            Nehal Machinery Limited
-          </h2>
-          <Link to="/machineries">
-            <Button
-              title="Watch More"
-              width="300px"
-              color="#000"
-              border="2px solid"
-              fontSize="40px"
-            />
-          </Link>
-        </center>
-      </div>
+      {loading ? (
+        <div className="loader">
+          <div class="loaderContainer ">
+            <div class="box1"></div>
+            <div class="box2"></div>
+            <div class="box3"></div>
+          </div>
+        </div>
+      ) : (
+        <>
+          {" "}
+          <div
+            style={{
+              paddingBottom: "30px",
+              position: "relative",
+            }}
+          >
+            <Carousel
+              additionalTransfrom={0}
+              arrows={true}
+              autoPlaySpeed={3000}
+              centerMode={false}
+              className=""
+              containerClass=""
+              dotListClass=""
+              draggable
+              focusOnSelect={false}
+              infinite
+              itemClass=""
+              keyBoardControl
+              minimumTouchDrag={80}
+              pauseOnHover
+              renderArrowsWhenDisabled={true}
+              renderButtonGroupOutside={false}
+              renderDotsOutside
+              responsive={{
+                desktop: {
+                  breakpoint: {
+                    max: 3000,
+                    min: 1024,
+                  },
+                  items: 1,
+                },
+                mobile: {
+                  breakpoint: {
+                    max: 464,
+                    min: 0,
+                  },
+                  items: 1,
+                },
+                tablet: {
+                  breakpoint: {
+                    max: 1024,
+                    min: 464,
+                  },
+                  items: 1,
+                },
+              }}
+              rewind={false}
+              rewindWithAnimation={false}
+              rtl={false}
+              shouldResetAutoplay
+              showDots
+              sliderClass=""
+              slidesToSlide={1}
+              swipeable
+            >
+              {banners.map((banner) => (
+                <div key={banner.id}>
+                  <img src={banner.img} className="bannerImg" alt="" />
+                </div>
+              ))}
+            </Carousel>
+          </div>
+          <div>
+            <center className="animate__animated animate__fadeInUp animate__slow bannerTextSection">
+              <h2 className="bannerText">
+                Welcome to <br />
+                Nehal Machinery Limited
+              </h2>
+              <Link to="/machineries">
+                <Button
+                  title="Watch More"
+                  width="300px"
+                  color="#000"
+                  border="2px solid"
+                  fontSize="40px"
+                />
+              </Link>
+            </center>
+          </div>
+        </>
+      )}
     </>
   );
 };
